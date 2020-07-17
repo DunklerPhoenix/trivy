6205271acb99c2fcb7fe37007c3f099dc4a5b5fb
Patrick Landschützer <patrick.landschuetzer@fabasoft.com>
2020-07-17 13:42:09 +0200

    add html template

--- pkg/report/writer_test.go
+++ pkg/report/writer_test.go
@@ -311,6 +311,294 @@ func TestReportWriter_Template(t *testing.T) {
 			template:      `{{ toLower (getEnv "AWS_ACCOUNT_ID") }} {{ getCurrentTime }}`,
 			expected:      `123456789012 2020-08-10T07:28:17.000958601Z`,
 		},
+		{
+			name: "html",
+			detectedVulns: []types.DetectedVulnerability{
+				{
+					VulnerabilityID:  "123",
+					PkgName:          "foo",
+					InstalledVersion: "1.2.3",
+					FixedVersion:     "3.3.5",
+					Vulnerability: dbTypes.Vulnerability{
+						Title:      "foobar",
+						Severity:   "LOW",
+						References: []string{"https://www.google.com"},
+					},
+				},
+				{
+					VulnerabilityID:  "456",
+					PkgName:          "bar",
+					InstalledVersion: "2.2.3",
+					FixedVersion:     "3.5.5",
+					Vulnerability: dbTypes.Vulnerability{
+						Title:      "barbar",
+						Severity:   "HIGH",
+						References: []string{"https://www.google.at"},
+					},
+				},
+				{
+					VulnerabilityID:  "789",
+					PkgName:          "bar",
+					InstalledVersion: "3.2.3",
+					FixedVersion:     "3.4.5",
+					Vulnerability: dbTypes.Vulnerability{
+						Title:      "foofoo",
+						Severity:   "HIGH",
+						References: []string{"https://www.google.biz"},
+					},
+				},
+			},
+			template: `<!DOCTYPE html>
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+{{- if . }}
+	<style>
+		* {
+		font-family: Arial, Helvetica, sans-serif;
+		}
+		h1 {
+		text-align: center;
+		}
+		.group-header th {
+		font-size: 200%;
+		}
+		.sub-header th {
+		font-size: 150%;
+		}
+		table, th, td {
+		border: 1px solid black;
+		border-collapse: collapse;
+		white-space: nowrap;
+		padding: .3em;
+		}
+		table {
+		margin: 0 auto;
+		}
+		.severity {
+		text-align: center;
+		font-weight: bold;
+		color: #fafafa;
+		}
+		.severity-LOW .severity { background-color: #5fbb31; }
+		.severity-MEDIUM .severity { background-color: #e9c600; }
+		.severity-HIGH .severity { background-color: #ff8800; }
+		.severity-CRITICAL .severity { background-color: #e40000; }
+		.severity-UNKNOWN .severity { background-color: #747474; }
+		.severity-LOW { background-color: #5fbb3160; }
+		.severity-MEDIUM { background-color: #e9c60060; }
+		.severity-HIGH { background-color: #ff880060; }
+		.severity-CRITICAL { background-color: #e4000060; }
+		.severity-UNKNOWN { background-color: #74747460; }
+		table tr td:first-of-type {
+		font-weight: bold;
+		}
+		.links a,
+		.links[data-more-links=on] a {
+		display: block;
+		}
+		.links[data-more-links=off] a:nth-of-type(1n+5) {
+		display: none;
+		}
+		a.toggle-more-links { cursor: pointer; }
+	</style>
+	<title>{{- escapeXML ( index . 0 ).Target }} - Trivy Report - {{ getCurrentTime }}</title>
+	<script>
+		window.onload = function() {
+		document.querySelectorAll('td.links').forEach(function(linkCell) {
+			var links = [].concat.apply([], linkCell.querySelectorAll('a'));
+			[].sort.apply(links, function(a, b) {
+			return a.href > b.href ? 1 : -1;
+			});
+			links.forEach(function(link, idx) {
+			if (links.length > 3 && 3 === idx) {
+				var toggleLink = document.createElement('a');
+				toggleLink.innerText = "Toggle more links";
+				toggleLink.href = "#toggleMore";
+				toggleLink.setAttribute("class", "toggle-more-links");
+				linkCell.appendChild(toggleLink);
+			}
+			linkCell.appendChild(link);
+			});
+		});
+		document.querySelectorAll('a.toggle-more-links').forEach(function(toggleLink) {
+			toggleLink.onclick = function() {
+			var expanded = toggleLink.parentElement.getAttribute("data-more-links");
+			toggleLink.parentElement.setAttribute("data-more-links", "on" === expanded ? "off" : "on");
+			return false;
+			};
+		});
+		};
+	</script>
+	</head>
+	<body>
+	<h1>{{- escapeXML ( index . 0 ).Target }} - Trivy Report - {{ getCurrentTime }}</h1>
+	<table>
+	{{- range . }}
+		<tr class="group-header"><th colspan="6">{{ escapeXML .Type }}</th></tr>
+		{{- if (eq (len .Vulnerabilities) 0) }}
+		<tr><th colspan="6">No Vulnerabilities found</th></tr>
+		{{- else }}
+		<tr class="sub-header">
+		<th>Package</th>
+		<th>Installed Version</th>
+		<th>Level</th>
+		<th>Vulnerability ID</th>
+		<th>Fixed Version</th>
+		<th>Links</th>
+		</tr>
+		{{- range .Vulnerabilities }}
+		<tr class="severity-{{ escapeXML .Vulnerability.Severity }}">
+		<td class="pkg-name">{{ escapeXML .PkgName }}</td>
+		<td class="pkg-version">{{ escapeXML .InstalledVersion }}</td>
+		<td class="severity">{{ escapeXML .Vulnerability.Severity }}</td>
+		<td>{{ escapeXML .VulnerabilityID }}</td>
+		<td>{{ escapeXML .FixedVersion }}</td>
+		<td class="links" data-more-links="off">
+			{{- range .Vulnerability.References }}
+			<a href={{ escapeXML . | printf "%q" }}>{{ escapeXML . }}</a>
+			{{- end }}
+		</td>
+		</tr>
+		{{- end }}
+		{{- end }}
+	{{- end }}
+	</table>
+{{- else }}
+	</head>
+	<body>
+	<h1>Trivy Returned Empty Report</h1>
+{{- end }}
+	</body>
+</html>`,
+			expected: `<!DOCTYPE html>
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<style>
+		* {
+		font-family: Arial, Helvetica, sans-serif;
+		}
+		h1 {
+		text-align: center;
+		}
+		.group-header th {
+		font-size: 200%;
+		}
+		.sub-header th {
+		font-size: 150%;
+		}
+		table, th, td {
+		border: 1px solid black;
+		border-collapse: collapse;
+		white-space: nowrap;
+		padding: .3em;
+		}
+		table {
+		margin: 0 auto;
+		}
+		.severity {
+		text-align: center;
+		font-weight: bold;
+		color: #fafafa;
+		}
+		.severity-LOW .severity { background-color: #5fbb31; }
+		.severity-MEDIUM .severity { background-color: #e9c600; }
+		.severity-HIGH .severity { background-color: #ff8800; }
+		.severity-CRITICAL .severity { background-color: #e40000; }
+		.severity-UNKNOWN .severity { background-color: #747474; }
+		.severity-LOW { background-color: #5fbb3160; }
+		.severity-MEDIUM { background-color: #e9c60060; }
+		.severity-HIGH { background-color: #ff880060; }
+		.severity-CRITICAL { background-color: #e4000060; }
+		.severity-UNKNOWN { background-color: #74747460; }
+		table tr td:first-of-type {
+		font-weight: bold;
+		}
+		.links a,
+		.links[data-more-links=on] a {
+		display: block;
+		}
+		.links[data-more-links=off] a:nth-of-type(1n+5) {
+		display: none;
+		}
+		a.toggle-more-links { cursor: pointer; }
+	</style>
+	<title>foojunit - Trivy Report - 2020-08-10T07:28:17.000958601Z</title>
+	<script>
+		window.onload = function() {
+		document.querySelectorAll('td.links').forEach(function(linkCell) {
+			var links = [].concat.apply([], linkCell.querySelectorAll('a'));
+			[].sort.apply(links, function(a, b) {
+			return a.href > b.href ? 1 : -1;
+			});
+			links.forEach(function(link, idx) {
+			if (links.length > 3 && 3 === idx) {
+				var toggleLink = document.createElement('a');
+				toggleLink.innerText = "Toggle more links";
+				toggleLink.href = "#toggleMore";
+				toggleLink.setAttribute("class", "toggle-more-links");
+				linkCell.appendChild(toggleLink);
+			}
+			linkCell.appendChild(link);
+			});
+		});
+		document.querySelectorAll('a.toggle-more-links').forEach(function(toggleLink) {
+			toggleLink.onclick = function() {
+			var expanded = toggleLink.parentElement.getAttribute("data-more-links");
+			toggleLink.parentElement.setAttribute("data-more-links", "on" === expanded ? "off" : "on");
+			return false;
+			};
+		});
+		};
+	</script>
+	</head>
+	<body>
+	<h1>foojunit - Trivy Report - 2020-08-10T07:28:17.000958601Z</h1>
+	<table>
+		<tr class="group-header"><th colspan="6">test</th></tr>
+		<tr class="sub-header">
+		<th>Package</th>
+		<th>Installed Version</th>
+		<th>Level</th>
+		<th>Vulnerability ID</th>
+		<th>Fixed Version</th>
+		<th>Links</th>
+		</tr>
+		<tr class="severity-LOW">
+		<td class="pkg-name">foo</td>
+		<td class="pkg-version">1.2.3</td>
+		<td class="severity">LOW</td>
+		<td>123</td>
+		<td>3.3.5</td>
+		<td class="links" data-more-links="off">
+			<a href="https://www.google.com">https://www.google.com</a>
+		</td>
+		</tr>
+		<tr class="severity-HIGH">
+		<td class="pkg-name">bar</td>
+		<td class="pkg-version">2.2.3</td>
+		<td class="severity">HIGH</td>
+		<td>456</td>
+		<td>3.5.5</td>
+		<td class="links" data-more-links="off">
+			<a href="https://www.google.at">https://www.google.at</a>
+		</td>
+		</tr>
+		<tr class="severity-HIGH">
+		<td class="pkg-name">bar</td>
+		<td class="pkg-version">3.2.3</td>
+		<td class="severity">HIGH</td>
+		<td>789</td>
+		<td>3.4.5</td>
+		<td class="links" data-more-links="off">
+			<a href="https://www.google.biz">https://www.google.biz</a>
+		</td>
+		</tr>
+	</table>
+	</body>
+</html>`,
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
